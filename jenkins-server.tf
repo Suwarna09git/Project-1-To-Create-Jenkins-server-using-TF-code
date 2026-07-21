e18d22b0c3c982b9d8210e9f1257c8a330e867a6
Suwarna09git <suwarna.kayarkar09@gmail.com>
2026-07-21 19:07:53 +0530

    Create jenkins-server.tf

---
+++ jenkins-server.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "latest-ubuntu-image" {
+    most_recent = true
+    owners      = ["099720109477"]                   #Canonical's AWS account ID
+
+       filter {
+        name = "name"
+        values = ["ubuntu/image/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+       }
+
+       filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+       }                 
+}
+resource "aws_instance" "myapp-server" {
+    ami                          = data.aws_ami.latest-ubuntu-image.id
+    instance_type                = var.instance_type
+    key_name                     = "jenkins-server"
+    subnet_id                    = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids       = [aws_default_security_group.default-sg.id]
+    availability_zone            = var.availability_zone
+    associate_public_ip_address  = true
+    user_data                    = file ("jenkins-server-script.sh")
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
