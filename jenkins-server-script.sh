3754401ff14888dc2490ff88de50e3b034234667
Suwarna09git <suwarna.kayarkar09@gmail.com>
2026-07-21 19:07:07 +0530

    Create jenkins-server-script.sh

---
+++ jenkins-server-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# update system
+sudo apt update -y
+sudo apt upgrade -y
+
+# install jenkins
+sudo apt install -y fontconfig openjdk-17-jre
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary / | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install -y jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+
+# install git
+sudo apt install -y git
+
+# install terraform
+sudo apt install -y gnupg software-properties-common
+wget -0- https://apt.release.hashicorp.com/gpg | \
+sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.release.hashicorp.com $(1sb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install -y terraform
