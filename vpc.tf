e2a18acc6daa0a769d51fcfc05968f692860cd62
Suwarna09git <suwarna.kayarkar09@gmail.com>
2026-09-07 13:50:32 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,9 +6,9 @@ resource "aws_vpc" "myapp-vpc" {
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id                         = aws_vpc.myapp-vpc.id
-    cidr_block                     = var.subnet_cidr_block
-    availability_zone              = var.avail_zone
+    vpc_id            = aws_vpc.myapp-vpc.id
+    cidr_block        = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
         Name = "${var.env_prefix}-subnet-1"
     }
