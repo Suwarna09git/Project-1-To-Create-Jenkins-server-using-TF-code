b779dff6485b3fa78efd16cce6db187640b9d451
Suwarna09git <suwarna.kayarkar09@gmail.com>
2026-09-07 13:44:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "young-minds-app-terraform-32"
+        bucket = "jenkins-server-terraform-32"
         region = "ap-south-1"
         key    = "project/terraform.tfstate"
     }
