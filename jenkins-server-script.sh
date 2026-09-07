a0eb491c5de2851d60bda49fdcc2335017f41bcd
Suwarna09git <suwarna.kayarkar09@gmail.com>
2026-09-07 13:45:28 +0530

    Refactor Jenkins installation and update Java setup

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -4,14 +4,8 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-# install Java 21 (Amazon Corretto)
-sudo apt install -y wget gnupg software-properties-common
-wget -O- https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
-sudo apt update -y
-sudo apt install -y java-21-amazon-corretto-jdk
-
-# install Jenkins
+# install jenkins
+sudo apt install fontconfig openjdk-21-jre -y
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
@@ -22,12 +16,16 @@ sudo apt install -y jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
+
 # install git
 sudo apt install -y git
 
 # install terraform
 sudo apt install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update -y
 sudo apt install -y terraform
