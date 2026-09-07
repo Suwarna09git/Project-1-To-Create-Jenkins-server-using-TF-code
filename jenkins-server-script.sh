a2c9d53c459725143fe9d6389a15deded5b8ef59
Suwarna09git <suwarna.kayarkar09@gmail.com>
2026-09-07 13:39:12 +0530

    Update Jenkins installation to use Java 21

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -4,28 +4,30 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-# install jenkins
-sudo apt install -y fontconfig openjdk-17-jre
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-/usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary / | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
+# install Java 21 (Amazon Corretto)
+sudo apt install -y wget gnupg software-properties-common
+wget -O- https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+sudo apt update -y
+sudo apt install -y java-21-amazon-corretto-jdk
+
+# install Jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y
 sudo apt install -y jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-
 
 # install git
 sudo apt install -y git
 
 # install terraform
 sudo apt install -y gnupg software-properties-common
-wget -0- https://apt.release.hashicorp.com/gpg | \
-sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.release.hashicorp.com $(1sb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update -y
 sudo apt install -y terraform
